cff0517f61e0d8a5afe3d95d8f9f4259039d8265
kexia <kexia@davidson.edu@compute0.ada.davidson.edu>
2026-05-28 10:20:48 -0400

    Add image and update documentation

---
+++ .ipynb_checkpoints/hello-checkpoint.py
@@ -0,0 +1,21 @@
+"""
+hello.py
+
+A tiny collaborative workflow exercise for practicing
+Git + GitHub research development workflows.
+
+author: Olivia
+date created: 05/28/2026
+"""
+
+def main():
+    """
+    Print a welcome mesage for the Git workflow activity.
+    """
+
+    print("Welcom to the collaborative Git workflow activity!")
+
+
+if __name__ == "__main__":
+    main()
+
